43833bf83a6c7419a6acc867061f360cc4f2a31c
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-09 15:46:59 -0300

    incluindo header

--- src/Pages/Cardapio/index.tsx
+++ src/Pages/Cardapio/index.tsx
@@ -8,6 +8,14 @@ export default function Cardapio() {
       <nav className={styles.menu}>
         <Logo/>
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa.
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      </section>
     </main>
   )
 }
