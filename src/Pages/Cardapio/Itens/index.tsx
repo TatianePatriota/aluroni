405175f8a7f377fa42837dd1d4450fd3346a02f2
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-13 19:32:39 -0300

    add sort do ordenador

--- src/Pages/Cardapio/Itens/index.tsx
+++ src/Pages/Cardapio/Itens/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import cardapio from "./itens.json";
 import Item from "./Item";
 import styles from "./Itens.module.scss";
@@ -9,10 +10,9 @@ interface Props {
   ordenador: string;
 }
 
-
 export default function Itens(props: Props) {
   const [lista, setLista] = useState(cardapio);
-  const { busca, filtro } = props;
+  const { busca, filtro, ordenador } = props;
 
   function testaBusca(title:string) {
     const regex = new RegExp(busca, 'i');
@@ -27,10 +27,24 @@ export default function Itens(props: Props) {
     }
   }
 
+  function ordenar(novaLista: typeof cardapio) {
+    switch(ordenador) {
+      case 'porcao': 
+        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      case 'qtd_pessoas':
+        return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
+      case 'preco':
+        return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
+      default:
+        return novaLista; 
+    }
+  }
+
   useEffect(() => {
     const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-    setLista(novaLista);
-  },[busca, filtro]);
+    setLista(ordenar(novaLista));
+  },[busca, filtro, ordenador, ordenar, testaBusca, testaFiltro])
+
 
   return (
     <div className={styles.itens}>
