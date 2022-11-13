c7f7dffd4b6f47c5419a20157ecb7acc07e8a8a9
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-11-13 11:08:36 -0300

    add props

--- src/Pages/Cardapio/Itens/Item/index.tsx
+++ src/Pages/Cardapio/Itens/Item/index.tsx
@@ -1,29 +1,32 @@
-import logo from "assets/logo.svg";
-import styles from "./Item.module.scss"
+import styles from "./Item.module.scss";
+import cardapio from "../itens.json";
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props) {
+  const { title, description, photo, category, size, serving, price} = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt="Imagem" />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2>Macarrão</h2>
-          <p>Descrição</p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
           <div className={styles.item__tipo}>
-            Massa
+            {category.label}
           </div>
           <div className={styles.item__porcao}>
-            500G
+            {size}g
           </div>
           <div className={styles.item__qtdpessoas}>
-            Serve 2 pessoas
+            Serve {serving} pessoa{serving === 1 ? "" : "s"}
           </div>
           <div className={styles.item__valor}>
-            R$: 50,00
+            R${price.toFixed(2)}
           </div>
         </div>
       </div>
