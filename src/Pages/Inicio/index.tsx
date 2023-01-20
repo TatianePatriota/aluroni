a92981cf1cd9cf45b212034bf7089dc5a2116d55
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-20 20:38:46 -0300

    add componente

---
+++ src/Pages/Inicio/index.tsx
@@ -0,0 +1,10 @@
+import cardapio from "../Cardapio/Itens/itens.json";
+
+export function Inicio() {
+  return (
+    <section>
+      <h3>Recomendações da Cozinha</h3>
+      <div></div>
+    </section>
+  );
+}
